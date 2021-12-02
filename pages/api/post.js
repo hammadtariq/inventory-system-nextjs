465ac358b78f1818f65529f6bbc4401e2686f9f3
xamqrexii <usama1630@gmail.com>
2021-12-02 13:56:33 +0500

    refactor(seqeulize): remove models

--- pages/api/post.js
+++
@@ -1,8 +0,0 @@
-import dbConnect from "../../lib/dbConnect";
-import Post from "../../models/post";
-
-export default async function user(req, res) {
-  await dbConnect();
-  const post = await Post.create({ title: "post one" });
-  res.send({ post });
-}
--- pages/api/user.js
+++
@@ -1,8 +0,0 @@
-import dbConnect from "../../lib/dbConnect";
-import User from "../../models/User";
-
-export default async function user(req, res) {
-  await dbConnect();
-  const user = await User.create({ name: "rehan" });
-  res.send({ user });
-}
