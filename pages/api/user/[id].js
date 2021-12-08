e874f2b57d3261e9bef32492bd977641b89755b3
xamqrexii <usama1630@gmail.com>
2021-12-08 22:56:31 +0500

    feat(user): delete user by primary key

--- pages/api/user/[id].js
+++ pages/api/user/[id].js
@@ -59,4 +59,37 @@ const updateUser = async (req, res) => {
   }
 };
 
-export default nextConnect().put(updateUser);
+const deleteUser = async (req, res) => {
+  // validate api fields
+  const { error, value } = apiSchema.validate({
+    id: req.query.id,
+  });
+
+  if (error && error && Object.keys(error).length) {
+    return res.status(400).send({ success: false, error });
+  }
+
+  try {
+    await db.dbConnect();
+    const user = await db.User.findByPk(value.id);
+
+    // if user not found
+    if (!user) {
+      return res
+        .status(404)
+        .send({ success: false, message: "User does not exist" });
+    }
+
+    // delete user
+    await db.User.destroy({ where: { id: value.id } });
+
+    return res.send({
+      success: true,
+      message: "User deleted succesfully",
+    });
+  } catch (error) {
+    res.status(500).send({ success: false, error });
+  }
+};
+
+export default nextConnect().put(updateUser).delete(deleteUser);
