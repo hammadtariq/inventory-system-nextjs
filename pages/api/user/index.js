9adbd84ebe618422bbf4815b3094920a7bc11a5e
xamqrexii <usama1630@gmail.com>
2021-12-09 19:24:50 +0500

    feat(user): get all users

---
+++ pages/api/user/index.js
@@ -0,0 +1,22 @@
+import db from "@/lib/postgres";
+import { apiHandler } from "@/lib/handler";
+import user from "models/user";
+
+const allUsers = async (_, res) => {
+  try {
+    await db.dbConnect();
+    const users = await db.User.findAll({
+      attributes: { exclude: ["password"] },
+    });
+
+    if (!users.length) {
+      return res.send({ success: true, message: "No user found", users });
+    }
+
+    res.send({ success: true, users });
+  } catch (error) {
+    res.status(500).send({ success: false, error });
+  }
+};
+
+export default apiHandler.get(allUsers);
