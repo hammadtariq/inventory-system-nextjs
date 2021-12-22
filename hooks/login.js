4d522a885871d837926acc061d2795b3ef9d8fed
xamqrexii <usama1630@gmail.com>
2021-12-22 15:45:32 +0500

    refactor(http-client): add errorStaus variable for future use

--- hooks/login.js
+++ hooks/login.js
@@ -1,3 +1,12 @@
+import useSWR from "swr";
+
 import { get, post, put, remove } from "@/lib/http-client";
+import { cache } from "joi";
+
+import router from "next/router";
+
+// const router = useRouter();
 
 export const loginUser = (data) => post("/api/user/login", data);
+
+export const verifyToken = () => get("/api/user/verify-token");
