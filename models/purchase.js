700211faf8cab00e3a457a43b2e78e87295cc028
dkukda <dkukda@nisum.com>
2021-12-28 16:12:35 +0500

    feat(create and get purchase order): create an api for creating purchase order and get all pO

--- models/purchase.js
+++ models/purchase.js
@@ -6,6 +6,9 @@ module.exports = (sequelize, DataTypes) => {
     static associate({ Company }) {
       this.belongsTo(Company, { foreignKey: "companyId" });
     }
+    toJSON() {
+      return { ...this.get(), companyId: undefined };
+    }
   }
   purchase.init(
     {
