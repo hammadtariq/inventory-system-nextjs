af00fc48ba94ac03f0daa28d4aea344fe31c95b0
OwaisRaza <owaisraza1989@gmail.com>
2022-01-19 13:14:09 +0500

    refactor pagination code

--- hooks/purchase.js
+++ hooks/purchase.js
@@ -4,14 +4,15 @@ import { get, post, put } from "@/lib/http-client";
 import { DEFAULT_PAGE_LIMIT } from "@/utils/ui.util";
 
 export const usePurchaseOrders = () => {
-  const [_limit, setLimit] = useState(DEFAULT_PAGE_LIMIT);
-  const [_offset, setOffset] = useState(0);
-  const { data, error, mutate } = useSWR(`/api/purchase?limit=${_limit}&offset=${_offset}`, get);
+  const [pagination, setPagination] = useState({ limit: DEFAULT_PAGE_LIMIT, offset: 0 });
+  const { data, error, mutate } = useSWR(`/api/purchase?limit=${pagination.limit}&offset=${pagination.offset}`, get);
 
-  const paginationHandler = useCallback((limit, offset) => {
-    setLimit(limit);
-    setOffset(offset);
-  }, []);
+  const paginationHandler = useCallback(
+    (limit, offset) => {
+      setPagination({ limit, offset });
+    },
+    [setPagination]
+  );
 
   return {
     purchaseOrders: data,
