8d46fd8c36ee16d54e06208141e6dceeefe922f0
talhaahmed09 <endhogaya3@gmail.com>
2022-09-01 20:07:14 +0500

    feat: filter feature implemented at select input boxes

--- components/selectCompany.js
+++ components/selectCompany.js
@@ -2,6 +2,7 @@ import { memo } from "react";
 import { Select, Alert } from "antd";
 
 import { useCompanyAttributes } from "@/hooks/company";
+import { selectSearchFilter } from "@/utils/filter.util";
 
 const { Option } = Select;
 
@@ -9,10 +10,17 @@ export default memo(function SelectCompany({ selectCompanyOnChange, ...props }) 
   const { company, isLoading, error } = useCompanyAttributes(["companyName", "id"]);
   if (error) return <Alert message={error} type="error" />;
 
+  const onSearch = (value, option) => {
+    console.log(value, option);
+    console.log(option.children.toLowerCase().includes(value.toLowerCase()));
+  };
+
   return (
     <Select
       {...props}
       loading={isLoading}
+      filterOption={selectSearchFilter}
+      optionFilterProp="children"
       showSearch
       placeholder="Search to Select Company"
       allowClear
