9ec50b440813d382c6c136ac10c9a07607001bea
talha ahmed <endhogaya3@gmail.com>
2022-11-08 21:43:20 +0500

    autocomplete options bug

--- components/SearchInput.js
+++ components/SearchInput.js
@@ -6,25 +6,27 @@ import _debounce from "lodash/debounce";
 import styles from "@/styles/SearchInput.module.css";
 
 const { Search } = Input;
-
+const Option = AutoComplete.Option;
 const searchResult = (results = [], valueKey = "") =>
-  results.map((result) => ({
-    value: result[valueKey],
-    label: (
-      <div key={result.id} className={styles.listItem}>
-        {result[valueKey]}
-      </div>
-    ),
-  }));
+  results.map((result) => {
+    return {
+      value: result[valueKey] + "-" + result.id,
+      label: (
+        <div key={result.id} className={styles.listItem}>
+          {result[valueKey]}
+        </div>
+      ),
+    };
+  });
 
 const SearchInput = ({ handleSearch, handleSelect, valueKey }) => {
-  const [options, setOptions] = useState([]);
-
+  const [results, setResults] = useState([]);
   const _handleSearch = async (value) => {
-    console.log("_handleSearch", value);
     if (value) {
+      value = value;
       const results = await handleSearch(value.toLowerCase());
-      setOptions(searchResult(results, valueKey));
+      setResults(results);
+      // setOptions(searchResult(results, valueKey));
     } else {
       handleSearch();
     }
@@ -32,7 +34,7 @@ const SearchInput = ({ handleSearch, handleSelect, valueKey }) => {
   };
 
   const _handleSelect = (itemId, option) => {
-    const id = option.label.key;
+    const id = option.key;
     handleSelect(id);
   };
 
@@ -41,11 +43,18 @@ const SearchInput = ({ handleSearch, handleSelect, valueKey }) => {
       <AutoComplete
         dropdownMatchSelectWidth={252}
         className={styles.inputWrap}
-        options={options}
         onSelect={_handleSelect}
         onSearch={_debounce(_handleSearch, 500)}
+        style={{
+          width: 200,
+        }}
       >
-        <Search size="large" placeholder="Search inventory" loading={false} allowClear enterButton />
+        {results.map((result) => (
+          <Option key={result.id} value={result[valueKey]}>
+            {result[valueKey]}
+          </Option>
+        ))}
+        {/* <Search size="large" placeholder="Search inventory" allowClear enterButton /> */}
       </AutoComplete>
     </>
   );
