646c6fb3920081f9290e4039e8ffda7eebde6feb
talha ahmed <endhogaya3@gmail.com>
2022-12-15 17:23:18 +0500

    fix: missing imports fixed

--- utils/query.utils.js
+++ utils/query.utils.js
@@ -1,4 +1,6 @@
 import Id from "pages/api/company/[id]";
+import { companySumQuery, customerSumQuery } from "query";
+import db from "@/lib/postgres";
 
 export const balanceQuery = async (id, queryType) => {
   let rawQuery;
