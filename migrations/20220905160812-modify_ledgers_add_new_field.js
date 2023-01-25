cfff253b95fd7f1630ac88a4815dc790094bb87a
muneeb <muneebshahzad244@gmail.com>
2023-01-25 19:02:40 +0500

    fixed ledger issue

--- migrations/20220905160812-modify_ledgers_add_new_field.js
+++ migrations/20220905160812-modify_ledgers_add_new_field.js
@@ -4,6 +4,8 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.addColumn("ledgers", "customerTotal", Sequelize.FLOAT);
     await queryInterface.addColumn("ledgers", "companyTotal", Sequelize.FLOAT);
+    await queryInterface.addColumn("ledgers", "totalBalance", Sequelize.FLOAT);
+    await queryInterface.addColumn("ledgers", "transactionId", Sequelize.INTEGER);
   },
 
   down: async (queryInterface) => {
